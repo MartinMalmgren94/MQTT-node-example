81a095a66e8aa5a42ed55662fc68524bd0882a6b
Martin Malmgren <martin.malmgren94@hotmail.com>
2021-11-19 14:31:59 +0100

    Added some comments.

--- publish.js
+++ publish.js
@@ -1,11 +1,12 @@
+// Importing mqtt
 const mqtt = require('mqtt');
-
+// Creating mqtt client connected to the broker
 var client = mqtt.connect("mqtt://broker.hivemq.com");
-
+// Client listen after event "connect"
 client.on("connect", () => { 
+    // When it's connected ever 3 sec there will be a random number that will publish.
     setInterval(() => {
         var random = Math.random() * 50;
-
         if(random < 30){
             client.publish("TestForAnNodeExample927118", "Hello MQTT random number less than 30: " + random)
         }
--- sub.js
+++ sub.js
@@ -1,11 +1,13 @@
+// Importing mqtt
 const mqtt = require("mqtt");
+// Creating mqtt client connected to the broker
 var client = mqtt.connect("mqtt://broker.hivemq.com")
-
+// Client listen when it have connected to the broker
 client.on("connect", () => {
     client.subscribe("TestForAnNodeExample927118");
     console.log("Success on sub.")
 })
-
+// Client listen when an message are published.
 client.on("message", (topic, message) => {
     console.log("message:")
     console.log(message.toString())
